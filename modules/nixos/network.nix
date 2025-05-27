810e1a1414b2a02fb8076490ecce2ad450413a11
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2025-05-27 21:34:31 +0200

    Remove resolved

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -5,19 +5,13 @@
 
     networkmanager = {
       enable = true;
+      dns = "none";
       wifi.powersave = true;
     };
 
     nameservers = [
-      "2a07:e340::5"
       "194.242.2.5"
     ];
-  };
-  services.resolved = {
-    enable = true;
-    dnsovertls = "true";
-    dnssec = "false";
-    domains = ["~."];
   };
   settings.users.groups = ["networkmanager"];
 }
