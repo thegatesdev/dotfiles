8e9fa4eb937cb8342540a4b60f13594349a21d2c
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2025-06-03 10:50:58 +0200

    Disable perlless for now

--- modules/nixos/clean.nix
+++ modules/nixos/clean.nix
@@ -1,10 +1,10 @@
 {modulesPath, ...}: {
-  imports = ["${modulesPath}/profiles/perlless.nix"];
+  # imports = ["${modulesPath}/profiles/perlless.nix"];
 
   environment.defaultPackages = [];
   documentation.man.enable = false;
   programs.nano.enable = false;
-  
+
   services.dbus.implementation = "broker";
   services.irqbalance.enable = true;
 }
