0b2ab207a1bd4cad1e1961ddd12910a848d44511
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2025-06-03 10:43:11 +0200

    Enable dbus broker and irqbalance

--- modules/nixos/clean.nix
+++ modules/nixos/clean.nix
@@ -4,4 +4,7 @@
   environment.defaultPackages = [];
   documentation.man.enable = false;
   programs.nano.enable = false;
+  
+  services.dbus.implementation = "broker";
+  services.irqbalance.enable = true;
 }
