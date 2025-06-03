b499fb11ae5da4ca82872fd2e7fd0b7d261f0b4c
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2025-06-03 10:10:47 +0200

    Enable perlless activation

--- modules/nixos/clean.nix
+++ modules/nixos/clean.nix
@@ -1,4 +1,7 @@
+{modulesPath, ...}:
 {
+  imports = [ "${modulesPath}/profiles/perlless.nix" ];
+
   environment.defaultPackages = [];
   documentation.man.enable = false;
   programs.nano.enable = false;
