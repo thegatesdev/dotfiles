9095bd5c037b7ad0c7ce61110e612d914cf61aac
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2025-06-09 13:29:16 +0200

    Switch Helix formatter to nixfmt-rfc-style

--- parts/home/helix.nix
+++ parts/home/helix.nix
@@ -9,7 +9,7 @@
       {
         name = "nix";
         auto-format = true;
-        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+        formatter.command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
       }
     ];
   };
