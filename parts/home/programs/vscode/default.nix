c4906180be92f583444e787437d5bbabb10c68f8
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2025-07-09 11:01:38 +0200

    Split out vscode plugin list

--- parts/home/programs/vscode/default.nix
+++ parts/home/programs/vscode/default.nix
@@ -5,7 +5,6 @@
   ...
 }: let
   extensions = inputs.nix-vscode-extensions.extensions.${system};
-  marketplace = extensions.vscode-marketplace;
 in {
   programs.vscode = {
     enable = true;
@@ -16,33 +15,9 @@ in {
       enableExtensionUpdateCheck = false;
       enableUpdateCheck = false;
       userSettings = import ./settings.nix;
-      extensions = with marketplace; [
-        angular.ng-template
-        astro-build.astro-vscode
-        activitywatch.aw-watcher-vscode
-        # beardedbear.beardedicons
-        # beardedbear.beardedtheme
-        biomejs.biome
-        editorconfig.editorconfig
-        emeryao.file-counter
-        bierner.markdown-preview-github-styles
-        bierner.markdown-emoji
-        bierner.markdown-checkbox
-        bierner.markdown-yaml-preamble
-        bierner.markdown-footnotes
-        bierner.markdown-mermaid
-        ecmel.vscode-html-css
-        davidanson.vscode-markdownlint
-        unifiedjs.vscode-mdx
-        misodee.vscode-nbt
-        jnoortheen.nix-ide
-        ziyasal.vscode-open-in-github
-        slevesque.shader
-        svelte.svelte-vscode
-        minecraftcommands.syntax-mcfunction
-        albert.tabout
-        vitest.explorer
-      ];
+      extensions = import ./plugins.nix {
+        marketplace = extensions.vscode-marketplace;
+      };
     };
   };
 }
---
+++ parts/home/programs/vscode/plugins.nix
@@ -0,0 +1,28 @@
+{marketplace}:
+with marketplace; [
+  angular.ng-template
+  astro-build.astro-vscode
+  activitywatch.aw-watcher-vscode
+  # beardedbear.beardedicons
+  # beardedbear.beardedtheme
+  biomejs.biome
+  editorconfig.editorconfig
+  emeryao.file-counter
+  bierner.markdown-preview-github-styles
+  bierner.markdown-emoji
+  bierner.markdown-checkbox
+  bierner.markdown-yaml-preamble
+  bierner.markdown-footnotes
+  bierner.markdown-mermaid
+  ecmel.vscode-html-css
+  davidanson.vscode-markdownlint
+  unifiedjs.vscode-mdx
+  misodee.vscode-nbt
+  jnoortheen.nix-ide
+  ziyasal.vscode-open-in-github
+  slevesque.shader
+  svelte.svelte-vscode
+  minecraftcommands.syntax-mcfunction
+  albert.tabout
+  vitest.explorer
+]
