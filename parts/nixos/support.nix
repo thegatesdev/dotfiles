b4d50e6748c812d5e11a1d8a0ee0108daecd3691
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2025-08-07 15:25:05 +0200

    Add i3wm support settings

--- parts/nixos/support.nix
+++ parts/nixos/support.nix
@@ -8,8 +8,20 @@ with lib; let
 in {
   options.settings.support = {
     swaywm = mkEnableOption "swaywm support";
+    i3wm = mkEnableOption "i3wm support";
   };
-  config = mkIf cfg.swaywm {
-    security.polkit.enable = true;
-  };
+  config = mkMerge [
+    (mkIf cfg.swaywm {
+      security.polkit.enable = true;
+    })
+    (mkIf cfg.i3wm {
+      services.xserver = {
+        enable = true;
+        windowManager.i3.enable = true;
+      };
+      services.displayManager = {
+        defaultSession = "none+i3";
+      };
+    })
+  ];
 }
